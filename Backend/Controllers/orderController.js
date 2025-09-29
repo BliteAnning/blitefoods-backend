052ba2f50cf2bb6d4d82056ed682139655192ee0
Bright Anning <anningbright578@gmail.com>
2025-09-29 18:26:23 +0000

    orderUpdate

--- Backend/Controllers/orderController.js
+++ Backend/Controllers/orderController.js
@@ -1,15 +1,12 @@
 import userModel from "../Models/userModel.js";
 import orderModel from "../Models/orderModel.js";
 import axios from "axios"
-//import Stripe from 'stripe'
-
-//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 //placing user order from  frontend
 const placeOrder = async (req, res) =>{
 
-    const frontend_url = "http://localhost:3000"
+
    //creating new order
     try {
         const newOrder = new orderModel({
@@ -27,14 +24,14 @@ const placeOrder = async (req, res) =>{
         let totalAmount = req.body.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
         totalAmount += 2; // Delivery charge
 
-        //get the user item for the stripe payment
+        //get the user item for the payment
         const paystackRes = await axios.post(
             "https://api.paystack.co/transaction/initialize",
             {
-                email: req.body.email, // Make sure email is sent from frontend
+                email: req.body.email, 
                 amount: totalAmount * 100, // Paystack expects amount in kobo
-                //reference: `order_${newOrder._id}`,
-                //callback_url: `${frontend_url}/verify?orderId=${newOrder._id}`
+                reference: `order_${newOrder._id}`,
+                callback_url: `${process.env.FRONTEND_URL}/verify?orderId=${newOrder._id}`
             },
             {
                 headers: {
@@ -54,4 +51,92 @@ const placeOrder = async (req, res) =>{
         res.json({ success: false, message: "error" });
     }
 }
-export  default placeOrder;
+
+
+const verifyOrder = async (req, res)=>{
+    const {orderId, reference} = req.body;
+    try {
+    // Verify with Paystack
+    const paystackRes = await axios.get(
+      `https://api.paystack.co/transaction/verify/${reference}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        },
+      }
+    );
+    if (paystackRes.data.data.status === "success") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "paid", error: false });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      throw new Error("Not paid");
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message || error,
+      error: true,
+    });
+  }
+}
+
+//user orders
+const userOrder = async (req, res) =>{
+    try {
+        const orders = await orderModel.find({userId: req.body.userId})
+        res.json({
+            success: true,
+            error:false,
+            data: orders
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            error: true,
+            message: error.message || error
+        })
+    }
+}
+
+//listing all orders at the admin panel
+const listOrder = async (req, res) =>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({
+            success: true,
+            error: false,
+            data: orders
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            error: true,
+            message: error.message || error
+        })
+    }
+}
+
+//api for updating order status
+const updateStatus = async (req, res)=>{
+    try {
+         await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status})
+         res.json({
+            success: true,
+            error: false,
+            message: "status update"
+         })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false, 
+            error: true,
+            message: error.message || error
+        })
+    }
+}
+
+
+export  {placeOrder, verifyOrder, userOrder, listOrder, updateStatus};
--- Backend/Routes/orderRouter.js
+++ Backend/Routes/orderRouter.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import authMiddleware from '../Middlewares/auth.js'
-import placeOrder from '../Controllers/orderController.js'
+import {listOrder, placeOrder, updateStatus, userOrder, verifyOrder} from '../Controllers/orderController.js'
 
 const orderRouter =express.Router();
 
 orderRouter.post("/place", authMiddleware,placeOrder)
+orderRouter.post("/verify", verifyOrder)
+orderRouter.post("/userorders",authMiddleware, userOrder)
+orderRouter.get("/list-order",listOrder)
+orderRouter.post("/status", updateStatus)
 
 export default orderRouter;
